e60b3a1b1c3b2f0ced32a5e634bcddcc01fda426
mahdi <313mmy313@gmail.com>
2024-05-15 18:30:08 +0330

    edit: urls.py updated by task and list urls.

--- taskulu/urls.py
+++ taskulu/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
 	path('admin/', admin.site.urls),
     path('token/refresh/', views.token_refresh),
     path('token/obtain/', views.token_obtain_pair),
-    # path('auth/token/', views.obtain_auth_token),
     path('account/', include('account.urls')),
 	path('board/' , include('board.urls')),
+    path('list/', include('task.urls')),
     # Schemas 
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
